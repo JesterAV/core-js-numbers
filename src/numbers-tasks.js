6d6976f6f45ae2d2921cb8110587a12c1b6e736b
Aleksei Bulash <winchesrers@gmail.com>
2025-09-16 23:22:49 +0300

    feat: implement getSumOfDigits functions

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -293,8 +293,10 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((sum, digit) => sum + parseInt(digit, 10), 0);
 }
 
 /**
